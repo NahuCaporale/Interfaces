16139cab1dcb748734e54370329784211923316f
Geronimo Moroni <geromoroniii@hotmail.com>
2025-10-06 21:39:22 -0300

    seccion juego, terminada. donaciones igual, solo falta corregir margen blanco que aparece en costados.

---
+++ .history/TP2/js/donaciones_20251006133659.js
@@ -0,0 +1,24 @@
+const icon = document.querySelector(".text-monto i");
+const helpText = document.getElementById("help-text");
+
+icon.addEventListener("click", () => {
+  // alterna mostrar/ocultar
+  if (helpText.style.display === "block") {
+    helpText.style.display = "none";
+  } else {
+    helpText.style.display = "block";
+  }
+});
+
+
+
+
+//switch boton mensual-solo esta vez
+const buttons = document.querySelectorAll(".frecuencia-buttons button");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    buttons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
